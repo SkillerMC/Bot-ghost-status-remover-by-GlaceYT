3f6075ec6e4cf3fe0bc93e7d9259daf8eb3f4347
SkillerMC <ghaithgamer09@gmail.com>
2024-10-15 13:13:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Playing **RoyaMC**"];
+const statusMessages = ["Playing **RoyalMC**"];
 const statusTypes = [ 'dnd' "RoyalMC"];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
